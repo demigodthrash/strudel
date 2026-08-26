c5fd80902bc0a286b75b2ff97f6069f0e2449490
Felix Roos <flix91@gmail.com>
2026-08-26 15:23:00 +0200

    update dough to 0.3.0 + add group, voices, glide

--- packages/dough/dough.mjs
+++ packages/dough/dough.mjs
@@ -4,11 +4,15 @@ Copyright (C) 2022 Strudel contributors - see <https://codeberg.org/uzu/strudel/
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { register, noteToMidi } from '@strudel/core';
+import { register, noteToMidi, registerControl } from '@strudel/core';
 import { Dough, doughsamples } from 'dough-synth';
 import { getAudioContext, ensureMinimalOutput } from '@strudel/webaudio';
 import wasm from 'dough-synth/dough.wasm?url';
 import workletCode from 'dough-synth/dough.js?raw';
+
+export const { group } = registerControl('group');
+export const { voices } = registerControl('voices');
+export const { glide } = registerControl('glide');
 
 Object.assign(globalThis, { doughsamples });
 
